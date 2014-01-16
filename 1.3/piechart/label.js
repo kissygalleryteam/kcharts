eb56b63a9429966e3ff841c6302679a9d6035da7
元晃 <yuanhuang.tjw@alibaba-inc.com>
2014-01-16 10:33:30 +0800

    修正饼图label靠的太近的问题

--- 1.3/piechart/label.js
+++ 1.3/piechart/label.js
@@ -234,10 +234,10 @@
          }
 
          if(bool_left){
-           x3 = x23 - size.width
-           y3 = y23 - size.height/2
+           x3 = x23 - size.width - 3; //左边也修正下，否者线条和文字靠的太近了
+           y3 = y23 - size.height/2;
          }else{
-           x3 = x23;
+           x3 = x23 + 5;  //右边修正下，否者线条和文字靠的太近了
            y3 = y23 - size.height/2;
          }
 
